034a2c1ace81106c2402e951cac3497a936a742c
BestGak <pro100gall@yandex.ru>
2021-08-06 14:07:24 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const questions = document.querySelectorAll('.faq__container-item');
+  questions.forEach(quest => {
+      quest.addEventListener('click', () => {
+          quest.classList.toggle('faq__container-item--active');
+          quest.nextElementSibling.classList.toggle('block');
+      });
+  });
+});
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+let bodyIndex = document.querySelector('body')
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+  bodyIndex.classList.toggle('overflow')
+})
+
+$('[data-fancybox="gallery"]').fancybox({
+  loop: true
+});
